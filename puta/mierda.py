88e73ff84cd14d44e609bc6e1b96f22bcd9c8938
e <ernesto@MacBook-Air-de-ernesto.local>
2018-09-30 04:54:53 -0500

    aceptado

--- puta/mierda.py
+++ puta/mierda.py
@@ -4,26 +4,28 @@ Created on 13/04/2018
 @author: ernesto
 '''
 
+# XXX: https://www.hackerearth.com/practice/algorithms/sorting/heap-sort/practice-problems/algorithm/divide-apples/
+
 N = int(input())
 n = [int(x) for x in input().strip().split(" ")]
 m = sum(n) // len(n)
 n_len = len(n)
 # print("n_len {}".format(n_len))
 #print("lista antes {}".format(n))
-#print("lista despues {}".format(n))
 n = list(map(lambda x:x - m, n))
+print("lista despues {}".format(n))
 r = 0
 b = [0] * n_len
 for i, x in enumerate(n[:-1]):
     b[i + 1] = b[i] + n[i]
     
-#print("b {}".format(b))
+print("b {}".format(b))
 
 b.sort()
 
 med = -b[n_len >> 1]
 
-#print("b {} med {}".format(b, med))
+print("b {} med {}".format(b, med))
 for bi in b:
     r += abs(bi + med)
 
